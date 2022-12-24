9e1de81def43f754ce15c317918935af50ae4d6a
Arvin Ding <dingarvin@gmail.com>
2022-12-24 14:31:46 -0800

    small changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <svg width="500" height="500" style={{border:"5px solid black"}}>
+      <RedSegment/>
+    </svg>
   );
 }
+function RedSegment (){
+  const [color, setColor] = useState("rgb(255,0,0)");
 
+  const [seg, setSeg] = useState({
+    x: "10",
+    y: "20",
+    display: true,
+  });
+  function moreWhite() {
+    setColor("rgb(255,200,200)");
+  }
+
+  function restoreColor() {
+    setColor("rgb(255,0,0)");
+  }
+  if(!seg.display)
+    return (null);
+  return (
+      <rect width="100" x={seg.x} y={seg.y} height="50" style={{fill:color,strokeWidth:"0",stroke:"rgb(0,0,0)"}}
+      onMouseEnter={moreWhite}
+      onMouseLeave={restoreColor}  />
+    );
+
+}
 export default App;
